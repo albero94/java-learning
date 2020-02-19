2be86582f9cddaa08f137012e49b8a22d4297153
albero94 <34701475+albero94@users.noreply.github.com>
2020-02-19 10:29:55 -0500

    feat: Create warrior class

---
+++ Java Derek Banas/tutorial/src/main/java/tutorial/Warrior.java
@@ -0,0 +1,35 @@
+package tutorial;
+
+public class Warrior {
+    protected String name = "Warrior";
+    public int health = 0;
+    public int attackMax = 0;
+    public int blockMax = 0;
+
+    public Warrior() {
+
+    }
+
+    public Warrior(String name, int health, int attackMax, int blockMax) {
+        this.setName(name);
+        this.health = health;
+        this.attackMax = attackMax;
+        this.blockMax = blockMax;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int attack(){
+        return 1 + (int)(Math.random() * ((attackMax = 1) + 1));
+    }
+
+    public int block(){
+        return 1 + (int)(Math.random() * ((blockMax = 1) + 1));
+    }
+}
